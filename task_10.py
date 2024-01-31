2c38a49d0fdcf2db50c5c11de466f376037acc47
torakristinel <106912120+torakristinel@users.noreply.github.com>
2024-01-31 11:08:20 +0100

    implemented task 10
    Co-authored-by: annetorgersen <annetorgersen@users.noreply.github.com>
    Co-authored-by: Aaryan Neupane <aaryanneupane@users.noreply.github.com>

--- task_10.py
+++ task_10.py
@@ -5,5 +5,6 @@ from utility.save_network import save_network
 loaded_network = load_network("networks/task_9_test, 5.json")
 
 print(loaded_network.fastest_route("1", "5", "Monday", 400))
+print(loaded_network.max_min("1", "2", "Monday"))
 
 # save_network(loaded_network)
