fd85bc1b8da5952eddd80fdd9c6dabad0c6f9697
aaryanneupane <aaryan200513@gmail.com>
2024-02-05 13:17:19 +0100

    Final changes.

--- tests/task_10_test.py
+++ tests/task_10_test.py
@@ -1,7 +1,10 @@
 from utility.load_network import load_network
 from utility.save_network import save_network
 
-'''This is a test for the fastest_route method. It loads a network from a file and finds the fastest route between two nodes.'''
+'''This is a test for the fastest_route and max_min methods.
+ It loads a network from a file and finds the fastest route between two nodes.
+ Furthermore, it calculates the maximum and minimum time between two nodes on a given day.'''
 
 loaded_network = load_network("networks/task_9_test, 5.json")
 print(loaded_network.fastest_route("1", "5", "Monday", 400))
+print(loaded_network.max_min("1", "2", "Monday"))
