5c2d4e9638dad0a9cad88dd907dd4fcd7d92d1d5
aaryanneupane <aaryan200513@gmail.com>
2024-04-12 11:20:00 +0200

    Implemented order handling.

--- assignment_3/order_management_test.py
+++ assignment_3/order_management_test.py
@@ -8,7 +8,6 @@ new_order = new_warehouse.add_order_to_warehouse("Anne")
 print(new_warehouse.get_remaining_orders())
 
 new_warehouse.handle_order(new_order)
-new_warehouse.animate_warehouse()
 
 print(new_warehouse.get_completed_order_list())
 print(new_warehouse.get_remaining_orders())
