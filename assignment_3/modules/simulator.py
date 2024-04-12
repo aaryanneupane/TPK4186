7fd2bd764cad873de163387f1d6a89d01c86c1fa
torakristinel <106912120+torakristinel@users.noreply.github.com>
2024-04-12 16:09:57 +0200

    simulator

--- assignment_3/modules/simulator.py
+++ assignment_3/modules/simulator.py
@@ -1,7 +1,10 @@
 from modules.warehouse import Warehouse
 from modules.robot import Robot
+import random
 
-class simulator:
+class Simulator:
+    client_list = ["Anne", "John", "Kari", "Lise", "Tora", "Lars", "Antoine", "Bjørn", "Helene", "Julie", "Johanne", "Erik"]
+
     def __init__(self, end_time:int) -> None:
         self.time = end_time
         self.current_time = 0
@@ -9,16 +12,16 @@ class simulator:
         
     def execute_simulation_loop(self):
         warehouse = Warehouse(1,3,10,1)
-        robots = warehouse.get_robots()
         for _ in range(self.time):
+            warehouse.handle_next_time_step()
             if (self.current_time%1000==0):
-                warehouse.add_order_to_warehouse("Anne")
-            if len(warehouse.get_remaining_orders()) > 0:
-                self.warehouse.handle_order(warehouse.get_remaining_orders[0])
-            for robot in robots:
-                robot.set_global_state()
-                robot.get_current_pos()
+                order_name = random.choice(Simulator.client_list)
+                warehouse.add_order_to_warehouse(order_name)
             self.current_time+=1
+            #for robot in robots:
+                #robot.set_global_state()
+                #robot.get_current_pos()
+           
 
    
 
