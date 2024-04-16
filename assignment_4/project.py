00465798c59f45a9432be7a955adf4697f96e3f2
Anne Torgersen <annetorg@stud.ntnu.no>
2024-04-16 10:55:10 +0200

    Data structure for assignment 4

--- assignment_4/project.py
+++ assignment_4/project.py
@@ -106,8 +106,52 @@ class Project:
     
     def _init_(self, id):
         self.id = id
-        self.nodes = []
+        self.nodes = dict()
         self.constraints = []
-        self.lanes = []
+        self.lanes = dict()
+        
+    def getId(self):
+        return self.id
+    
+    def setId(self, id):
+        self.id = id
+    
+    def getNodes(self):
+        return self.nodes.values()
+    
+    def lookForNode(self, id):
+        return self.nodes.get(id, None)
+        
+    def newGate(self, id):
+        gate = Gate(id)
+        self.nodes[id] = gate
+        return gate
+    
+    def newTask(self, id):
+        task = Task(id)
+        self.nodes[id] = task
+        return task
+    
+    def getConstraints(self):
+        return self.constraints
+    
+    def newConstraint(self, sourceNode, targetNode):
+        constraint = Constraint(sourceNode, targetNode)
+        self.constraints.append(constraint)
+        return constraint
+    
+    def getLanes(self):
+        return self.lanes.values()
+    
+    def lookForLane(self, id):
+        return self.lanes.get(id, None)
+    
+    def newLane(self, id):
+        lane = Lane(id)
+        self.lanes[id] = lane
+        return lane
+    
+    
+        
     
            
